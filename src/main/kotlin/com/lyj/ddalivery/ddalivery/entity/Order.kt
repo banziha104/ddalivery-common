61abe804b71cfdec0a3fe0d1a3da3620aaa192ac
iyeongjun <banziha104@gmail.com>
2020-02-05 17:25:57 +0900

    병합전 커밋

--- src/main/kotlin/com/lyj/ddalivery/ddalivery/entity/Order.kt
+++ src/main/kotlin/com/lyj/ddalivery/ddalivery/entity/Order.kt
@@ -2,9 +2,9 @@ package com.lyj.ddalivery.ddalivery.entity
 
 import javax.persistence.*
 
-@Entity
-data class Order(
-        @GeneratedValue(strategy = GenerationType.IDENTITY) @Id val orderId : Long,
-        @Column var clientId : Long, // 주문자명
-        @Column var productId : Long // 아이템명
-) 
+//@Entity
+//data class Order(
+//        @GeneratedValue(strategy = GenerationType.IDENTITY) @Id val orderId : Long,
+//        @Column var clientId : Long, // 주문자명
+//        @Column var productId : Long // 아이템명
+//)
--- src/main/kotlin/com/lyj/ddalivery/ddalivery/entity/Product.kt
+++ src/main/kotlin/com/lyj/ddalivery/ddalivery/entity/Product.kt
@@ -9,13 +9,13 @@ import javax.persistence.Id
 /***
  * 상품 엔티티
  */
-@Entity
-data class Product(
-        @GeneratedValue @Id val productId : Long,
-        @Column(length = 100, nullable = false) val name : String,
-        @Column(nullable = false) val image : String,
-        @Column(nullable = false) val price : Int,
-
-        @Column val seller : String
-
-)
+//@Entity
+//data class Product(
+//        @GeneratedValue @Id val productId : Long,
+//        @Column(length = 100, nullable = false) val name : String,
+//        @Column(nullable = false) val image : String,
+//        @Column(nullable = false) val price : Int,
+//
+//        @Column val seller : String
+//
+//)
