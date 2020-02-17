2bbc738df4543b7ef1d78f3fe8fde8cae38af0c1
iyeongjun <banziha104@gmail.com>
2020-02-17 18:19:27 +0900

    Order 변경

--- src/main/kotlin/com/lyj/ddalivery/ddalivery/entity/Order.kt
+++ src/main/kotlin/com/lyj/ddalivery/ddalivery/entity/Order.kt
@@ -13,7 +13,14 @@ import javax.persistence.*
 @Table(name = "order_table")
 data class Order(
         @GeneratedValue(strategy = GenerationType.IDENTITY) @Id val orderId : Long?,
-        @Column val productId : Long, // 아이템명
-        @Column val quantity : Int,
+        @Column(nullable = false)  val productId : Long, // 아이템명
+        @Column(length = 100, nullable = false) val productName: String,
+        @Column(nullable = false) val image: String,
+        @Column(nullable = false) val description: String,
+        @Column(nullable = false) val price: Int,
+        @Column(nullable = false) val quantity : Int,
+        @Column(nullable = false) val sellerName : String,
+        @Column(nullable = false) val sellerAddress : String,
+        @Column(nullable = false) val categoryName : String,
         @ManyToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY) @JoinColumn(name = "order_group_id") var orderGroup : OrderGroup? = null
 )
--- src/main/kotlin/com/lyj/ddalivery/ddalivery/entity/OrderGroup.kt
+++ src/main/kotlin/com/lyj/ddalivery/ddalivery/entity/OrderGroup.kt
@@ -6,13 +6,15 @@ import javax.persistence.*
 @Entity
 @Table(name = "order_group")
 data class OrderGroup(
-        @GeneratedValue(strategy = GenerationType.IDENTITY) @Id val orderGroupId : Long?,
-        @Column val clientName : String,
-        @Column val address : String,
-        @Column val latitude : Double,
-        @Column val longitude : Double,
-        @Column val totalPrice : Int,
-        @OneToMany(mappedBy = "orderGroup") val orders : List<Order>? = null
-) : AuditModel<Long>(){
+        @GeneratedValue(strategy = GenerationType.IDENTITY) @Id val orderGroupId: Long?,
+        @Column(nullable = false) val clientName: String,
+        @Column(nullable = false) val address: String,
+        @Column(nullable = false) val latitude: Double,
+        @Column(nullable = false) val longitude: Double,
+        @Column(nullable = false) val totalPrice: Int,
+        @Column(nullable = false) var isMatched: Boolean,
+        @Column(nullable = false) var isCompleted: Boolean,
+        @OneToMany(mappedBy = "orderGroup") val orders: List<Order>? = null
+) : AuditModel<Long>() {
     override fun getId(): Long? = orderGroupId
 }
